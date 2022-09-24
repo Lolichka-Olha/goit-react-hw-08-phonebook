777c4ac6833e48a8ce99dea4bafb61fa39ea5b40
Lolichka-Olha <94307228+Lolichka-Olha@users.noreply.github.com>
2022-09-24 14:08:53 +0000

    Deploying to gh-pages from @ Lolichka-Olha/goit-react-hw-08-phonebook@78ed06d61c97a4f2b223ff1428cc57d5281b3913 🚀

---
+++ static/js/983.99212167.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,o){o.r(t),o.d(t,{default:function(){return s}});var c=o(1552),n=o(7700),r=o(184);function s(){return(0,r.jsxs)(c.l3,{children:[(0,r.jsx)("h1",{children:"Welcome to phonebook!"}),(0,r.jsx)(c.E3,{children:(0,r.jsx)(c.Ei,{src:n,alt:"phonebook",width:"320"})})]})}},7700:function(e,t,o){e.exports=o.p+"static/media/istockphoto-177503800-612x612.56a5d17cfd30966ce700.jpg"}}]);
+//# sourceMappingURL=983.99212167.chunk.js.map
